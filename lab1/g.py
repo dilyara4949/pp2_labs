cc9cfaddf729f2b680c96cd78365d759c37383a4
dilyara4949 <90037377+dilyara4949@users.noreply.github.com>
2022-01-31 17:26:27 +0600

    Update g.py

--- lab1/g.py
+++ lab1/g.py
@@ -1,12 +1,11 @@
-bin = input()
+bin = int(input())
 
 # def convert(bin):
 #     return int(bin, 2)
 # print(convert(bin))
-
-dec, n, j = 0, len(bin), 0
-def to_dec(bin, dec, n, j):
-    if n == j:
-        return dec
-    return  int(bin[n-1-j]) * (2 ** j) + to_dec(bin, dec, n, j+1)
-print(to_dec(bin, dec, n, j))
+def to_dec(bin, j):     # j - степень
+    if bin == 1 or bin == 0:
+        return bin * (2 ** j)
+    last = bin % 10
+    return  last * (2 ** j) + to_dec(bin//10, j+1)
+print(to_dec(bin, 0))
