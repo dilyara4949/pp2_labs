c311d5cbb44521a7200e50ce1def64dfbc711f16
dilyara4949 <90037377+dilyara4949@users.noreply.github.com>
2022-02-06 21:55:36 +0600

    Update e.py

--- lab2/e.py
+++ lab2/e.py
@@ -1,25 +1,20 @@
 s = input()
 try:  #  when input in one line
     n, x = map(int, s.split())
-    arr = []
+    ans = 0
 
     for i in range(n):
-        arr.append(x + 2*i)
+        ans ^= x + 2*i
 
-    ans = arr[0]
-    for i in range(1, n):
-        ans ^= arr[i]
     print(ans)
 
 except:  # when input in two lines
     n = int(s.strip())
     x = int(input())
-    arr = []
+    ans = 0
 
     for i in range(n):
-        arr.append(x + 2*i)
+        ans ^= x + 2*i
 
-    ans = arr[0]
-    for i in range(1, n):
-        ans ^= arr[i]
     print(ans)
+
